ac433197024f47f0a656f8debc208e00d7a24acf
Mahou-Coder <forbidden.black.flames@gmail.com>
2016-10-05 09:01:14 +0530

    Update update
    some tweaks

---
+++ Foss---Even-Odd.py
@@ -0,0 +1,8 @@
+print('Welcome, I don't know you, but I'll tell you if a number is even or odd')
+a=int(input('Enter a number: '))
+if a==0:
+ print(a, 'is neither even nor odd')
+elif a%2==0:
+ print(a,'is even')
+else:
+ print(a,' is odd')
